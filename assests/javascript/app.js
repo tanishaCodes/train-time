662be507414d422fc5d923b6f31c04169d0180d3
Tanisha Howell <thowell@protonmail.com>
2020-01-26 02:47:27 -0500

    updated js and html

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,32 +1,96 @@
+// FIREBASE CONFIGURATION & LIBRARY
+var firebaseConfig = {
+    apiKey: "AIzaSyCwVJrOhlryspeim54o0v5D4Aelm9d6F6Y",
+    authDomain: "train-time-a363e.firebaseapp.com",
+    databaseURL: "https://train-time-a363e.firebaseio.com",
+    projectId: "train-time-a363e",
+    storageBucket: "train-time-a363e.appspot.com"
+};
+
+// INITIALIZING FIREBASE
+firebase.initializeApp(firebaseConfig);
+
+// Create a variable to reference the database.
+var dataRef = firebase.database();
 
 
+// CAPTURE THE BUTTON CLICK
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    // GRABBING USER INPUT FROM TEXT BOXES
+    var train = $("#train-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var time = $("#time-input").val().trim();
+    var tfrequency = $("#tfrequency-input").val().trim();
+
+    //CREATES LOCAL "TEMP" OBJECT FOR HOLDING TRAIN DATA
+    var newTrain = {
+        train: train,
+        destination: destination,
+        time: time,
+        tfrequency: tfrequency,
+    };
+
+    //LOGING NEW VARIABLES TO THE CONSOLE
+   console.log(newTrain.train);
+   console.log(newTrain.destination);
+   console.log(newTrain.time);
+   console.log(newTrain.tfrequency);
+
+// Code for handling the push
+    dataRef.ref().push(newTrain)
+
+//CLEARS OUT THE TEXT-BOXES
+$("#train-input").val("");
+$("#destination-input").val("");
+$("#time-input").val("");
+$("#tfrequency-input").val("");
+
+return false;
+
+});
+
+// FIREBASE EVENT TO A TRAINS TO THE DATABASE
+dataRef.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+//STORE SNAPSHOT ITEMS IN A VARIABLE
+var train = childSnapshot.val().train;
+var destination = childSnapshot.val().destination;
+var time = childSnapshot.val().time;
+var tfrequency = childSnapshot.val().tfrequency;
+var nextTrain = childSnapshot.val().nextTrain;
+var tMinutesTillTrain = childSnapshot.val().tMinutesTillTrain;
 
 //MOMENT.JS; CONVERSION CODE FOR THE MINUTES & ARRIVAL TIME//
-var tFrequency = 3;
+var tfrequency = 4;
+// THE TIME IS 00:03
+var firstTime = "00:03";
 
-    // Time is 3:30 AM
-    var firstTime = "03:30";
+// First Time (pushed back 1 year to make sure it comes before current time)
+var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+// Current Time
+var currentTime = moment();
 
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+// Difference between the times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+// Time apart (remainder)
+var tRemainder = diffTime % tFrequency;
 
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
+// Minute Until Train
+var tMinutesTillTrain = tFrequency - tRemainder;
 
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+// Next Train
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    //APPEND TO THE NEW ROW 
+    $("#current-train-schedule > tbody").append("<tr><td>" + train + "<tr><td>" + destination + "<tr><td>" +  time + "<tr><td>" + tfrequency + "<tr><td>" + nextTrain + "<tr><td>" + tMinutesTillTrain);
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
